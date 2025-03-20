9c073151ecdabe61d3febf1d0e540d697532856c
BC <bc.codes1@gmail.com>
2025-03-21 00:07:16 +0800

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,31 +2,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'techxbc',
-			social: {
-				//github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Artificial Intelligence',
-					autogenerate: { directory: 'articifial intelligence' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'techxbc',
+          social: {
+              //github: 'https://github.com/withastro/starlight',
+          },
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+              {
+                  label: 'Artificial Intelligence',
+                  autogenerate: { directory: 'articifial intelligence' },
+              },
+          ],
+      }),
 	],
+
+  adapter: cloudflare(),
 });
