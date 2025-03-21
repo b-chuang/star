68f742c226f98fd894880fb59f56e27e0f15c364
BC <bc.codes1@gmail.com>
2025-03-21 10:45:42 +0800

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,33 +4,33 @@ import starlight from '@astrojs/starlight';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'techxbc',
-          social: {
-              //github: 'https://github.com/withastro/starlight',
+  integrations: [starlight({
+      title: 'techxbc',
+      social: {
+          //github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+          {
+              label: 'Guides',
+              items: [
+                  // Each item here is one entry in the navigation menu.
+                  { label: 'Example Guide', slug: 'guides/example' },
+              ],
           },
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-              {
-                  label: 'Artificial Intelligence',
-                  autogenerate: { directory: 'artificial' },
-              },
-          ],
-      }),
-	],
+          {
+              label: 'Reference',
+              autogenerate: { directory: 'reference' },
+          },
+          {
+              label: 'Artificial Intelligence',
+              autogenerate: { directory: 'artificial' },
+          },
+      ],
+  }), markdoc()],
 
   adapter: cloudflare(),
 });
